e1ee3261049c9be2ee9893e39522cbe9e1eab767
gioiascari <gioia.ascari96@gmail.com>
2022-05-04 16:46:37 +0200

    font-family aggiunto

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php 
 
     include('database.php');
+    
+    
 
 //  var_dump($db);
  foreach($db as $key){
@@ -19,3 +21,18 @@
 <div>
    
 </div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <style>
+
+    </style>
+</body>
+</html>
