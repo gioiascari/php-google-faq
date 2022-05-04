a357a9f9d204dc28e7ad5137b4f9bc1722ec4d1a
gioiascari <gioia.ascari96@gmail.com>
2022-05-04 19:43:15 +0200

    sistemato

--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <title>Google Faq</title>
 </head>
+<body>
 <header>
         <div class="container-fluid">
             <div class="row p-2">
@@ -61,7 +62,7 @@
         </div>
         
     </header>
-<body>
+
     
 
     
